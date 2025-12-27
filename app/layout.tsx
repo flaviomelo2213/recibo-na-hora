6bfccd0dd284fe79b2ba3c4d0e01016fe22b6a8c
flaviomelo11 <flaviomelo11@gmail.com>
2025-12-27 02:45:10 +0000

    feat: 🚀 Configura Google AdSense no projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         {/* Google AdSense */}
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4754892182690500"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4754892182698508"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
